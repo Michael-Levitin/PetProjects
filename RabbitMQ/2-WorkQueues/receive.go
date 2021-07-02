ccfa3db3e827f5c5e735371b14b4bd909a8a6f95
Michael Levitin <michael9levi@gmail.com>
2021-07-02 12:51:27 +0300

    RabbitMQ - Distributing tasks among workers

---
+++ RabbitMQ/2-WorkQueues/receive.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"task_queue", // name
+		// durable - queue will restart from saved point if RabbitMQ node restarts
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	FailOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(
+		// making queue of 1 for each consumer - faster
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	FailOnError(err, "Failed to set QoS")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-acknowledgement
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	FailOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+	// receive.go simulates work - by sleeping 1 sec for each dot in the message
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+			d.Ack(false) // instead of setting autoAck
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
