a1c69a33ddc988863e9ccd4557e29777f25f97e0
Michael Levitin <michael9levi@gmail.com>
2021-07-02 14:52:59 +0300

    RabbitMQ - Publish/Subscribe

---
+++ RabbitMQ/3-Publish-Subscribe/emit_log.go
@@ -0,0 +1,62 @@
+// Publish/Subscribe - Sending messages to many consumers at once
+// Exchange doesn't care whether anyone listens - old messages are discarded
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"logs",   // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	FailOnError(err, "Failed to declare an exchange")
+
+	body := bodyFrom(os.Args)
+	err = ch.Publish(
+		"logs", // exchange
+		"",     // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	FailOnError(err, "Failed to publish a message")
+
+	log.Printf(" [x] Sent %s", body)
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	return s
+}
