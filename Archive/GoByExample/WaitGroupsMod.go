9152ef0c08a78dd6cb21bdb077793ea91ca25d70
Michael Levitin <michael9levi@gmail.com>
2021-08-18 13:01:02 +0300

    WaitGroupsMod.go working, running ratelimiter(former dosome) as goroutine in main Added atomic counter for jobs and reset

--- Archive/GoByExample/WaitGroupsMod.go
+++ Archive/GoByExample/WaitGroupsMod.go
@@ -1,17 +1,20 @@
+// package ratelimiter
 package main
 
 import (
 	"fmt"
 	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type job func() // тип функция
 
-func worker(id int, jobs <-chan job, wg *sync.WaitGroup) {
+func worker(id int, jobs <-chan job, wg *sync.WaitGroup, cPM *uint64) {
 	defer wg.Done()
 	for fn := range jobs {
+		atomic.AddUint64(cPM, 1)
 		//fmt.Println("worker", id, "started de job")
 		fn()
 		//fmt.Println("worker", id, "finished da job")
@@ -20,47 +23,54 @@ func worker(id int, jobs <-chan job, wg *sync.WaitGroup) {
 }
 
 func main() {
+
 	var wg sync.WaitGroup
-	current := time.Now()
-	wg.Add(1)
-	go ratelimiter(&wg)
-	wg.Wait()
-	fmt.Println("Total time: ", time.Since(current))
-}
-
-//func ratelimiter(ch chan job, totalWorker int, ratePerM int) {
-
-func ratelimiter(wg *sync.WaitGroup) {
-	defer wg.Done()
-	const numJobs = 5
-	jobs := make(chan job, numJobs)
-	//results := make(chan struct{}, numJobs)
+	jobs := make(chan job, 4)
 
 	rand.Seed(time.Now().UnixNano() + rand.Int63())
 	sliceJobs := make([]job, 3) // Слайс функций для нагрузки
 	sliceJobs[0] = func() {     // одну переменный типа job
-		current := time.Now()
+		//current := time.Now()
 		time.Sleep(time.Duration(rand.Intn(1e2)) * time.Millisecond)
-		fmt.Println("Hello", time.Since(current))
+		//fmt.Println("Hello", time.Since(current))
 	}
 	sliceJobs[1] = func() { // вторую
-		current := time.Now()
+		//current := time.Now()
 		time.Sleep(time.Duration(rand.Intn(2e2)) * time.Millisecond)
-		fmt.Println("Golang", time.Since(current))
+		//fmt.Println("Golang", time.Since(current))
 	}
 	sliceJobs[2] = func() { // и еще одну
-		current := time.Now()
+		//current := time.Now()
 		time.Sleep(time.Duration(rand.Intn(3e2)) * time.Millisecond)
-		fmt.Println("World", time.Since(current))
+		//fmt.Println("World", time.Since(current))
 	}
 
-	for w := 1; w <= 5; w++ {
-		wg.Add(1)
-		go worker(w, jobs, wg)
-	}
-
-	for i := 0; i < 10; i++ {
+	current := time.Now()
+	wg.Add(1)
+	go rateLimiter(jobs, 5, 220, &wg)
+	for i := 0; i < 1000; i++ {
 		jobs <- sliceJobs[rand.Intn(3)]
+	} // передаем функции
+	close(jobs) // закрытие канала - сигнал для завершения
+	wg.Wait()   // ждем когда все "рабочие" закончат
+	fmt.Println("Total time: ", time.Since(current))
+}
+
+func rateLimiter(jobs chan job, totalWorkers int, ratePerM int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var cpM uint64 // counterPerM
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			fmt.Println(cpM)
+			atomic.CompareAndSwapUint64(&cpM, cpM, 0)
+		}
+	}() // resetting the counter
+
+	for w := 1; w <= totalWorkers; w++ {
+		wg.Add(1)
+		go worker(w, jobs, wg, &cpM)
 	}
-	close(jobs)
 }
