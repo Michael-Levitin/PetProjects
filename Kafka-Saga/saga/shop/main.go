38c0216e0d50c934db84ed85be4209e456fec964
Michael Levitin <stgriard@yandex.ru>
2022-09-30 22:19:54 +0300

    Saga - shop: moved main to a func and goroutine

--- Kafka-Saga/saga/shop/main.go
+++ Kafka-Saga/saga/shop/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"golang.org/x/net/context"
 	"log"
 	"math/rand"
 	"time"
@@ -11,7 +12,7 @@ import (
 	"kafka-saga/saga/consts"
 )
 
-func main() {
+func NewShop(ctx context.Context) error {
 	cfg := sarama.NewConfig()
 	cfg.Producer.Return.Successes = true
 	syncProducer, err := sarama.NewSyncProducer(consts.Brokers, cfg)
@@ -19,32 +20,47 @@ func main() {
 		log.Fatalf("sync kafka: %v", err)
 	}
 
-	for {
-		d := consts.Order{
-			Id:   int(time.Now().UnixNano()),
-			Data: time.Now().Format("order 150405.000"),
-		}
-		b, err := json.Marshal(d)
-		if err != nil {
-			log.Printf("wtf? %v", err)
-			continue
-		}
-		par, off, err := syncProducer.SendMessage(&sarama.ProducerMessage{
-			Topic: "income_orders",
-			Key:   sarama.StringEncoder(fmt.Sprintf("%v", d.Id)),
-			Value: sarama.ByteEncoder(b),
-		})
-		log.Printf("order %v -> %v; %v", par, off, err)
-
-		time.Sleep(time.Millisecond * 500)
-
-		if rand.Intn(10) == 9 {
-			par, off, err = syncProducer.SendMessage(&sarama.ProducerMessage{
-				Topic: "reset_orders",
+	go func() {
+		for {
+			d := consts.Order{
+				Id:   int(time.Now().UnixNano()),
+				Data: time.Now().Format("order 150405.000"),
+			}
+			b, err := json.Marshal(d)
+			if err != nil {
+				log.Printf("marshal unsuccessful %v", err)
+				continue
+			}
+			par, off, err := syncProducer.SendMessage(&sarama.ProducerMessage{
+				Topic: "reserve_orders",
 				Key:   sarama.StringEncoder(fmt.Sprintf("%v", d.Id)),
 				Value: sarama.ByteEncoder(b),
 			})
-			log.Printf("reset %v -> %v; %v", par, off, err)
+			log.Printf("order %v -> %v; %v", par, off, err)
+
+			time.Sleep(time.Millisecond * 500)
+
+			if rand.Intn(10) == 9 {
+				par, off, err = syncProducer.SendMessage(&sarama.ProducerMessage{
+					Topic: "order_reset",
+					Key:   sarama.StringEncoder(fmt.Sprintf("%v", d.Id)),
+					Value: sarama.ByteEncoder(b),
+				})
+				log.Printf("reset %v -> %v; %v", par, off, err)
+			}
 		}
+	}()
+
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	err := NewShop(ctx)
+	if err != nil {
+		log.Fatalf("NewStock: %v", err)
 	}
+	<-ctx.Done()
 }
