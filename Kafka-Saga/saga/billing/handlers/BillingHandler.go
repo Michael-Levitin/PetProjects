6497a4a3d92300b1f58bd5e186a49386b4186f40
Michael Levitin <stgriard@yandex.ru>
2022-10-03 22:18:01 +0300

    Saga - billing - added bill reset

--- Kafka-Saga/saga/billing/handlers/BillingHandler.go
+++ Kafka-Saga/saga/billing/handlers/BillingHandler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Shopify/sarama"
 	"kafka-saga/saga/consts"
 	"log"
+	"math/rand"
 )
 
 type BillingHandler struct {
@@ -29,6 +30,19 @@ func (b *BillingHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim
 			continue
 		}
 		log.Printf("billing reports - recieved payment for order %v: %v", d.Id, err)
+
+		if rand.Intn(10) == 1 {
+			_, _, err := b.P.SendMessage(&sarama.ProducerMessage{
+				Topic: "billing_order_reset",
+				Key:   sarama.StringEncoder(fmt.Sprintf("%v", d.Id)),
+				Value: sarama.ByteEncoder(msg.Value),
+			})
+			if err != nil {
+				log.Printf("Cant send reset: %v", err)
+			}
+			log.Printf("billing resets order %v", d.Id)
+			continue
+		}
 
 		_, _, err = b.P.SendMessage(&sarama.ProducerMessage{
 			Topic: "bill_confirmed",
